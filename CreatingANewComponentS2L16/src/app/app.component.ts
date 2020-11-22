88df20a5267cbef0805642c90d2b72a661b2e282
Kevin <tlk999@live.co.uk>
2020-11-22 16:03:56 +0000

    Component Styling Section 2 Lecture 21

--- CreatingANewComponentS2L16/src/app/app.component.ts
+++ CreatingANewComponentS2L16/src/app/app.component.ts
@@ -3,7 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css', './servers/servers.component.css'] // use multiple style sheets
+  // style: `p { color: red }` - also possible to use inline style rather than url-based style
 })
 export class AppComponent {
   title = 'CreatingANewComponentS2L16';
