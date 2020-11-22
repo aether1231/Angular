26ecb8b9f2044a66478ad6d0d82d33b0408885a0
Kevin <tlk999@live.co.uk>
2020-11-22 16:11:50 +0000

    Component selectors by attribute or class

--- CreatingANewComponentS2L16/src/app/servers/servers.component.ts
+++ CreatingANewComponentS2L16/src/app/servers/servers.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-servers',
+  selector: '[app-servers]', // select by attribute
+  //selector: '.app-server',    - select by class  
   templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css']
 })
