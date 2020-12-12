fc81acdb3a8a4a5d0735270a96afc0edcb563d55
Kevin <tlk999@live.co.uk>
2020-12-12 13:33:51 +0000

    Built In Directives - ngIf

---
+++ builtInDirectives/src/app/server/server.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-server',
+  templateUrl: './server.component.html',
+  styleUrls: ['./server.component.css']
+})
+export class ServerComponent implements OnInit {
+  serverId: number = 10;
+  serverStatus: string = 'offline';
+  allowAddServer = false;
+  serverCreationStatus = 'No server was created';
+  serverName = 'TestServer';
+  serverCreated = false;
+  constructor() {
+    setTimeout(() => {
+      this.allowAddServer = true;
+    }, 2000);
+  }
+
+  getServerStatus() {
+    return this.serverStatus;
+  }
+
+  onCreateServer() {
+    this.serverCreated = true;
+    this.serverCreationStatus = 'Server was created. Name is ' + this.serverName;
+  }
+  onInputServerName(event) {
+    this.serverName = (<HTMLInputElement>event.target).value;
+  }
+  ngOnInit(): void {
+  }
+
+}
